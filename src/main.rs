63c419bc82fe4c3d308a59415a654b2775ca4875
Taylor Bergquist <knexer@gmail.com>
2023-10-12 11:09:28 -0700

    Add some little cubes that you can boop around with physics.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 mod mischief;
 
@@ -20,14 +21,17 @@ use mischief::{MischiefPlugin, poll_events, MischiefEvent, MischiefEventData};
 // Assign each cursor to a hand (i.e. click LMB to assign to left hand, RMB to assign to right hand) (done)
 // Capture and hide the OS cursor (done)
 // Press escape to quit (done)
-// Make two rigid bodies that fall from the top of the screen
+// Make two rigid bodies that fall from the top of the screen (done)
 // Make the bodies dangle from the cursors
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(MischiefPlugin)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugins(RapierDebugRenderPlugin::default())
         .add_systems(Startup, (spawn_camera, hide_os_cursor))
+        .add_systems(Startup, spawn_test_bodies)
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Update, spawn_left_cursor().run_if(not(any_with_component::<LeftCursor>())))
         .add_systems(Update, spawn_right_cursor().run_if(not(any_with_component::<RightCursor>())))
@@ -54,17 +58,18 @@ fn spawn_camera(mut commands: Commands) {
 struct LeftCursor;
 
 fn spawn_left_cursor() -> impl Fn(Commands, EventReader<MischiefEvent>) {
-    spawn_cursor::<LeftCursor>(Vec2::new(-100.0, 0.0), 0)
+    spawn_cursor::<LeftCursor>(Vec2::new(-200.0, 0.0), 0)
 }
 
 #[derive(Component, Default)]
 struct RightCursor;
 
 fn spawn_right_cursor() -> impl Fn(Commands, EventReader<MischiefEvent>) {
-    spawn_cursor::<RightCursor>(Vec2::new(100.0, 0.0), 1)
+    spawn_cursor::<RightCursor>(Vec2::new(200.0, 0.0), 1)
 }
 
 fn spawn_cursor<T>(start_pos: Vec2, button_id: u32) -> impl Fn(Commands, EventReader<MischiefEvent>) where T: Component + Default {
+    let cursor_size = 20.0;
     return move |mut commands: Commands, mut mouse_events: EventReader<MischiefEvent>| {
         for event in mouse_events.iter() {
             match event.event_data {
@@ -74,11 +79,13 @@ fn spawn_cursor<T>(start_pos: Vec2, button_id: u32) -> impl Fn(Commands, EventRe
                             SpriteBundle {
                                 transform: Transform::from_xyz(start_pos.x, start_pos.y, 0.0),
                                 sprite: Sprite {
-                                    custom_size: Some(Vec2::splat(10.0)),
-                                    ..Default::default()
+                                    custom_size: Some(Vec2::splat(cursor_size)),
+                                    ..default()
                                 },
-                                ..Default::default()
+                                ..default()
                             },
+                            RigidBody::KinematicPositionBased,
+                            Collider::cuboid(cursor_size / 2.0, cursor_size / 2.0),
                             Cursor(event.device),
                             T::default()
                         ));
@@ -89,6 +96,28 @@ fn spawn_cursor<T>(start_pos: Vec2, button_id: u32) -> impl Fn(Commands, EventRe
             }
         }
     };
+}
+
+fn spawn_test_bodies(mut commands: Commands) {
+    let positions = vec![
+        Vec2::new(-200.0, 300.0),
+        Vec2::new(200.0, 300.0),
+    ];
+    let body_size = 20.0;
+    for position in positions {
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(position.x, position.y, 0.0),
+                sprite: Sprite {
+                    custom_size: Some(Vec2::splat(body_size)),
+                    ..default()
+                },
+                ..default()
+            },
+            RigidBody::Dynamic,
+            Collider::cuboid(body_size / 2.0, body_size / 2.0),
+        ));
+    }
 }
 
 fn move_cursors(
